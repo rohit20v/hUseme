f9baec388fb534f83494697e2b0b779575e4065d
rohit <verma.rohit.1203@gmail.com>
2024-12-26 22:02:26 +0100

    Added save drawing as png button

--- src/components/SaveTool.tsx
+++ src/components/SaveTool.tsx
@@ -1,17 +1,25 @@
 import Tool from "./Tool.tsx";
 import {BsSave} from "react-icons/bs";
+import React from "react";
+import {Stage} from "konva/lib/Stage";
 
-export const SaveTool = (props: { onClick: () => any }) => {
+export const SaveTool = ({onClick, stageRef}: { onClick: () => void, stageRef: React.RefObject<Stage> }) => {
 
     const exportDrawing = () => {
-        console.log("exporting the drawing")
+        const imageUrl = stageRef.current.toDataURL();
+        const link = document.createElement("a");
+        link.download = "image.png"
+        link.href = imageUrl
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     }
 
     return <div onClick={exportDrawing} style={{boxShadow: "0 4px 16px 0 gray", borderRadius: "8px"}}>
         <Tool isSelected={false}>
             <BsSave
                 color={"black"} size={22}
-                onClick={props.onClick}/>
+                onClick={onClick}/>
         </Tool>
     </div>
 
