1df5befd0dfa840881ef577aca566cd5fa2bef8d
rohit <verma.rohit.1203@gmail.com>
2024-12-27 15:25:54 +0100

    Changed icon

--- src/components/ColorPickerTool.tsx
+++ src/components/ColorPickerTool.tsx
@@ -1,12 +1,12 @@
-import {PiPaintBucketLight} from "react-icons/pi";
 import Tool from "./Tool.tsx";
 import {ColorPicker} from "react-pick-color";
+import {IoIosColorFill} from "react-icons/io";
 
 export const ColorPickerTool = ({color, onToggle, isVisible, onChange}) => (
     <Tool isSelected={false}>
-        <PiPaintBucketLight color={color} size={22} onClick={onToggle}/>
+        <IoIosColorFill color={color} size={22} onClick={onToggle}/>
         {isVisible && (
-            <div style={{position: 'absolute', zIndex: 1000}}>
+            <div style={{position: 'absolute', zIndex: 2}}>
                 <ColorPicker color={color} onChange={onChange}/>
             </div>
         )}
